6fc95b29192fd65b2b16574ece6f20ba64ab1b59
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-05-12 14:40:03 -0400

    update to where-am-i-really.bat

--- files/where-am-i-really.bat
+++ files/where-am-i-really.bat
@@ -2,11 +2,26 @@
 echo.
 echo.
 echo ------
-echo Checked out from:
+echo Cloned from:
 git config --get remote.origin.url
+echo ------
 echo.
-echo Branch:
-git rev-parse --abbrev-ref HEAD
+echo ------
+:: get the branch
+git rev-parse --abbrev-ref HEAD > temp.txt
+set /p branch=<temp.txt
+echo Branch: %branch%
+:: get the date
+git show -s --format=%%ci > temp.txt
+set /p date=<temp.txt
+echo Date:   %date%
+:: get the hash
+git show -s --format=%%H > temp.txt
+set /p sha=<temp.txt
+echo SHA:    %sha%
+:: clean up
+del temp.txt
+:: done
 echo ------
 echo.
 echo.
